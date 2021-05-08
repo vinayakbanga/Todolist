dac7ce9a7eda76a5ec8b87d9d7b413eabe9d8968
Vinayak <vinayakbanga22@gmail.com>
2021-05-08 13:22:23 +0530

    Add git ignore, procfile and updated ports

--- app.js
+++ app.js
@@ -238,8 +238,12 @@ List.findOne({name: customListName},function (err,foundList) {
 //      res.render("about");
 // });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Server started on port 3000.");
 });
